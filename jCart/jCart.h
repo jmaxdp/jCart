d2c75bd187143d2a66cd3f558309681f401ecd71
jmaxdp <jerry.maxey@appanapp.com>
2016-11-11 13:18:15 -0800

    Initial Commit

---
+++ jCart/jCart.h
@@ -0,0 +1,19 @@
+//
+//  jCart.h
+//  jCart
+//
+//  Created by CRS on 11/11/16.
+//  Copyright © 2016 Jerry Maxey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for jCart.
+FOUNDATION_EXPORT double jCartVersionNumber;
+
+//! Project version string for jCart.
+FOUNDATION_EXPORT const unsigned char jCartVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jCart/PublicHeader.h>
+
+
